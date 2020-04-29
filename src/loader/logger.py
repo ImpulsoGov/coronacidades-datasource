e2f53ad638ca860ef1c4f633277f827f511ce4db
João Carabetta <joao.carabetta@gmail.com>
2020-04-29 11:04:23 -0300

    Update logger.py

--- src/loader/logger.py
+++ src/loader/logger.py
@@ -4,20 +4,21 @@ import os
 
 # Set the webhook_url to the one provided by Slack when you create the webhook at https://my.slack.com/services/new/incoming-webhook/
 def post_slack(error_args, status):
-
+    
+    webhook_url = os.getenv("SLACK_WEBHOOK")
+    
     if status == 'fail':
-        webhook_url = "https://hooks.slack.com/services/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"
         slack_data = {
             "text": "*Não!*\n\n*Origem:* {origin}\n*Tipo de erro:* {error_type}\n*Erro:* ```{error}```".format(
                 **error_args
             )
         }
     
-    if status == 'okay':
-        webhook_url = "https://hooks.slack.com/services/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"
-        slack_data = {
-            "text": "Sim! Tudo sob controle :D"
-        }
+#     if status == 'okay':
+
+#         slack_data = {
+#             "text": "Sim! Tudo sob controle :D"
+#         }
 
     response = requests.post(
         webhook_url,

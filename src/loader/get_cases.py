0c8ab01138266cb6fa09f2b32df25b46456febd8
Fernanda Scovino <fscovinom@gmail.com>
2020-05-04 19:07:09 -0300

    change get_cases to get daily notification rate

--- src/loader/get_cases.py
+++ src/loader/get_cases.py
@@ -2,99 +2,79 @@ import pandas as pd
 import datetime
 import numpy as np
 
+def _get_notification_ratio(df, config, place_col):
+    """
+    Calculate city/state notification rate = CFR * I(t) / D(t)
+    
+    1. Get daily 1/death_ratio = I(t) / D(t)
+    2. Calculate mavg of 7 days = CFR / death_ratio
+    """
+    
+    cfr = config["br"]["seir_parameters"]["fatality_ratio"]
+    
+    rate = df.groupby([place_col, "last_updated"]).sum()\
+            .reset_index()\
+            .set_index("last_updated", drop=True)\
+            .groupby(place_col)\
+            .apply(lambda x: x["confirmed_cases"] / x["deaths"])\
+            .rolling(window=7, min_periods=1)\
+            .apply(lambda x: np.mean(x) * cfr, raw=True)\
+            .reset_index() 
+
+    return rate
+
+def _adjust_subnotification_cases(df, config):
+
+    # Calcula taxa de notificação por cidade / estado
+    df_city = _get_notification_ratio(df, config, "city_id")\
+        .rename({0: "city_notification_rate"}, axis=1)
+    df_state = _get_notification_ratio(df, config, "state")\
+        .rename({0: "state_notification_rate"}, axis=1)
+    
+    df = df.merge(df_city, on=["city_id", "last_updated"])\
+           .merge(df_state, on=["state", "last_updated"])
+    
+    # Escolha taxa de notificação para a cidade: caso sem mortes, usa taxa UF
+    df["notification_rate"] = df["city_notification_rate"]
+    df["notification_rate"] = np.where(df["notification_rate"].isnull(), 
+                                       df["state_notification_rate"],
+                                       df["city_notification_rate"])
+    # Ajusta caso taxa > 1:
+    df["notification_rate"] = np.where(df["notification_rate"] > 1, 
+                                        1, 
+                                        df["notification_rate"])
+    
+    return df[["city_id", "state_notification_rate", "notification_rate", "last_updated"]].drop_duplicates()
+
 
 def _get_active_cases(df, window_period, cases_params):
-
+    
     # Soma casos diários dos últimos dias de progressão da doença
-    daily_active_cases = (
-        df.set_index("last_updated")
-        .groupby("city_id")["daily_cases"]
-        .rolling(min_periods=1, window=window_period)
-        .sum()
-        .reset_index()
-    )
+    daily_active_cases = df.set_index("last_updated")\
+                        .groupby("city_id")["daily_cases"]\
+                        .rolling(min_periods=1, window=window_period)\
+                        .sum().reset_index()
 
     df = df.merge(
-        daily_active_cases, on=["city_id", "last_updated"], suffixes=("", "_sum")
+        daily_active_cases, 
+        on=["city_id", "last_updated"], 
+        suffixes=("", "_sum")
     ).rename(columns=cases_params["rename"])
 
     return df
 
-
-def _get_notification_rate(group, config):
-
-    # 1a morte reportada nos últimos 7 dias ou ainda não reportada: -1
-    if np.any(group["deaths"] == 0):
-        return -1
-
-    daily_adjust = group["deaths"] / config["br"]["seir_parameters"]["fatality_ratio"]
-    notification_rate = np.mean(group["confirmed_cases"] / daily_adjust)
-
-    # nao tem subnotificacao de casos => taxa = 100%
-    if notification_rate > 1:
-        notification_rate = 1
-
-    return notification_rate
-
-
-def _adjust_subnotification_cases(df, cases_params, config):
-
-    # Filtra dataframe dos dias para o cálculo
-    notification_day = df["last_updated"].max() - datetime.timedelta(
-        cases_params["notification_window_days"]
-    )
-    df = df[df["last_updated"] > notification_day]
-
-    # Calcula taxa de notificação por cidade
-    city_notif_rate = (
-        df.groupby("city_id")
-        .apply(lambda x: _get_notification_rate(x, config))
-        .reset_index()
-        .rename({0: "city_notification_rate"}, axis=1)
-    )
-
-    # Calcula taxa de notificação por estado
-    state_notif_rate = (
-        df.groupby(["state", "last_updated"])
-        .sum()
-        .reset_index()
-        .groupby("state")
-        .apply(lambda x: _get_notification_rate(x, config))
-        .reset_index()
-        .rename({0: "state_notification_rate"}, axis=1)
-    )
-
-    df = df.merge(city_notif_rate, on="city_id").merge(state_notif_rate, on="state")
-
-    # Escolha taxa de notificação para a cidade: caso sem mortes (-1), usa taxa UF
-    df["notification_rate"] = df["city_notification_rate"]
-    df["notification_rate"] = np.where(
-        df["notification_rate"] == -1,
-        df["state_notification_rate"],
-        df["city_notification_rate"],
-    )
-
-    return df[
-        ["city_id", "state_notification_rate", "notification_rate"]
-    ].drop_duplicates()
-
-
-def now(country, config):
+def now(country, config, last=True):
 
     if country == "br":
         df = pd.read_csv(config[country]["cases"]["url"])
-        df = (
-            df.query('place_type == "city"').dropna(subset=["city_ibge_code"]).fillna(0)
-        )
+        df = df.query("place_type == 'city'").dropna(subset=["city_ibge_code"]).fillna(0)
 
         cases_params = config["br"]["cases"]
         df = df.rename(columns=cases_params["rename"])
         df["last_updated"] = pd.to_datetime(df["last_updated"])
 
-        infectious_period = (
-            config["br"]["seir_parameters"]["severe_duration"]
-            + config["br"]["seir_parameters"]["critical_duration"]
-        )
+        infectious_period = config["br"]["seir_parameters"]["severe_duration"] + \
+                            config["br"]["seir_parameters"]["critical_duration"]
 
         # Calcula casos ativos
         df = _get_active_cases(df, infectious_period, cases_params).rename(
@@ -103,14 +83,18 @@ def now(country, config):
 
         # Ajusta subnotificação de casos
         df = df.merge(
-            _adjust_subnotification_cases(df, cases_params, config), on="city_id"
+            _adjust_subnotification_cases(df, config), 
+            on=["city_id", "last_updated"]
         )
 
         df["active_cases"] = round(
-            df["infectious_period_cases"] / df["notification_rate"], 0
+            df["infectious_period_cases"] / df["notification_rate"], 
+            0
         )
 
-        df = df[df["is_last"] == True].drop(cases_params["drop"], 1)
+        if last:
+            df = df[df["is_last"] == True].drop(cases_params["drop"], 1)
+        
         df["city_id"] = df["city_id"].astype(int)
 
     return df

709e5b3e42b1b3528ffa7b56cf7c522a76ba07ad
Fernanda Scovino <fscovinom@gmail.com>
2020-05-18 16:32:04 -0300

    clean script

--- src/loader/endpoints/get_inloco_states.py
+++ src/loader/endpoints/get_inloco_states.py
@@ -1,22 +1,11 @@
 import pandas as pd
-
 from utils import secrets
 
 
 def now(config):
-    """This method is going to be called by main.py and it should return the output
-    DataFrame.
-
-    Parameters
-    ----------
-    config : dict
-    """
-
     return pd.read_csv(secrets(["inloco", "states", "url"]))
 
 
-# Output dataframe tests to check data integrity. This is also going to be called
-# by main.py
 TESTS = {
     "data is not pd.DataFrame": lambda df: isinstance(df, pd.DataFrame),
 }

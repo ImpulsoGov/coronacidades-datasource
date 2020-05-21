79c8e97409616e204f6789ea6d8151485cea4157
Joao Carabetta <joao.carabetta@gmail.com>
2020-05-21 11:48:03 -0300

    run jobs when start scheduler

--- src/loader/scheduler.py
+++ src/loader/scheduler.py
@@ -21,15 +21,19 @@ def handler(endpoint):
         print(e)
 
 
-endpoints = yaml.load(open("endpoints.yaml", "r"), Loader=yaml.FullLoader)
-
-tl = Timeloop()
-for e in endpoints:
-    tl._add_job(
-        func=handler,
-        interval=timedelta(minutes=e["update_frequency_minutes"]),
-        endpoint=e,
-    )
-
 if __name__ == "__main__":
+
+    endpoints = yaml.load(open("endpoints.yaml", "r"), Loader=yaml.FullLoader)
+
+    tl = Timeloop()
+    for e in endpoints:
+        tl._add_job(
+            func=handler,
+            interval=timedelta(minutes=e["update_frequency_minutes"]),
+            endpoint=e,
+        )
+
+    for job in tl.jobs:
+        job.execute(*job.args, **job.kwargs)
+
     tl.start(block=True)

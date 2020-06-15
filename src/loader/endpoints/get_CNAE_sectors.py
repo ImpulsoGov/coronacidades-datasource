ae9394ef8b621452cca319318a1daa2da8040a38
Fernanda Scovino <fscovinom@gmail.com>
2020-06-15 17:10:24 -0300

    change null test to all df + organize

--- src/loader/endpoints/get_CNAE_sectors.py
+++ src/loader/endpoints/get_CNAE_sectors.py
@@ -6,17 +6,21 @@ from numpy import dtype
 
 @allow_local
 def now(config, country="br"):
-    updates = download_from_drive(config[country]["drive_paths"]["CNAE_sectors"])
-    # updates = download_from_drive("https://docs.google.com/spreadsheets/d/1pP8dY9kRa9EvX3KcVPfMWJ8exDaoT1_bBggSKfD3y0k")
+    updates = (
+        download_from_drive(config[country]["drive_paths"]["CNAE_sectors"])
+        .assign(cnae=lambda df: df["cnae"].astype("int64"))
+        .assign(essential=lambda df: df["essential"].astype("bool"))
+    )
+
     return updates
 
 
 TESTS = {
     "data is not pd.DataFrame": lambda df: isinstance(df, pd.DataFrame),
-    "at least 74 sectors": lambda df: len(df) >= 74,
-    "is essential field contains null": lambda df: df["essential"].isna().sum() == 0,
+    "data without 74 sectors": lambda df: len(df) == 74,
+    "data contains null datafield": lambda df: df.isna().sum().sum() == 0,
     "CNAE field is not exclusively ints": lambda df: df["cnae"].dtype == dtype("int64"),
-    "is essential field is not exclusively bools": lambda df: df["essential"].dtype
+    "essential field is not exclusively bool": lambda df: df["essential"].dtype
     == dtype("bool"),
 }
 

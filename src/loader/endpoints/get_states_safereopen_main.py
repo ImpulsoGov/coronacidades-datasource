1dc4ce5576d1b25dd5f211b5df70fd7746b748d6
fernandascovino <fscovinom@gmail.com>
2020-07-09 14:49:53 +0000

    fix config url path

--- src/loader/endpoints/get_states_safereopen_main.py
+++ src/loader/endpoints/get_states_safereopen_main.py
@@ -7,11 +7,8 @@ from numpy import dtype
 @allow_local
 def now(config, country="br"):
 
-    # TODO: change in config
-    config["br"]["safereopen"]["rename"]["uf"] = "state_num_id"
-
     return (
-        download_from_drive(config[country]["drive_paths"]["reopening_data"])
+        download_from_drive(config[country]["drive_paths"]["br_states_reopening_data"])
         .rename(config["br"]["safereopen"]["rename"], axis=1)
         .assign(state_num_id=lambda df: df["state_num_id"].astype("int64"))
         .assign(cnae=lambda df: df["cnae"].astype("int64"))

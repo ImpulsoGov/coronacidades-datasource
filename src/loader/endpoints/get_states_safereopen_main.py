bc73a9f1d2460889356b0da407c0b43848fe9697
fernandascovino <fscovinom@gmail.com>
2020-07-10 17:07:53 +0000

    get new config path

--- src/loader/endpoints/get_states_safereopen_main.py
+++ src/loader/endpoints/get_states_safereopen_main.py
@@ -7,12 +7,8 @@ from numpy import dtype
 @allow_local
 def now(config, country="br"):
 
-    # TODO: change in config
-    config["br"]["safereopen"]["rename"]["uf"] = "state_num_id"
-
     return (
-        download_from_drive(config[country]["drive_paths"]["reopening_data"])
-        .rename(config["br"]["safereopen"]["rename"], axis=1)
+        download_from_drive(config[country]["drive_paths"]["br_states_reopening_data"])
         .assign(state_num_id=lambda df: df["state_num_id"].astype("int64"))
         .assign(cnae=lambda df: df["cnae"].astype("int64"))
     )
@@ -22,7 +18,7 @@ TESTS = {
     "data is not pd.DataFrame": lambda df: isinstance(df, pd.DataFrame),
     "data contains null datafield": lambda df: df.isnull().sum().sum() == 0,
     "CNAE field is not exclusively ints": lambda df: df["cnae"].dtype == dtype("int64"),
-    "UF field is not exclusively ints": lambda df: df["state_num_id"].dtype
+    "UF id field is not exclusively ints": lambda df: df["state_num_id"].dtype
     == dtype("int64"),
 }
 

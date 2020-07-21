1c292967533ec5709792936bbdcbb0c4cce0e0d6
fernandascovino <fscovinom@gmail.com>
2020-07-21 21:38:32 +0000

    fix columns

--- src/loader/endpoints/get_health.py
+++ src/loader/endpoints/get_health.py
@@ -4,52 +4,51 @@ from endpoints.helpers import allow_local
 from endpoints import get_places_id
 
 
-def _read_cities_data(country, config):
+def _read_df_data(country, config):
 
     tables = ["cities_population", "health_infrastructure"]
 
-    return {
+    dfs = {
         name: download_from_drive(config[country]["drive_paths"][name])
         for name in tables
     }
+
+    df = pd.merge(
+        dfs["cities_population"],
+        dfs["health_infrastructure"],
+        on="city_id",
+        how="left",
+        suffixes=("", "_y"),
+    )
+    return df.drop([c for c in df.columns if "_y" in c], axis=1)
 
 
 @allow_local
 def now(config, country="br"):
 
-    cities = _read_cities_data(country, config)
-    cities = pd.merge(
-        cities["cities_population"],
-        cities["health_infrastructure"],
-        on="city_id",
-        how="left",
-        suffixes=("", "_y"),
+    df = _read_df_data(country, config)
+    places_ids = get_places_id.now(config).assign(
+        city_id=lambda df: df["city_id"].astype(int)
     )
 
-    matchs = {
-        "city_name": "city_id",
-        "state_name": "state_id",
-        "state_num_id": "state_id",
-        "health_region_id": ["health_system_region", "state_id"],
-    }
-    cities = match_place_id(
-        cities.drop([c for c in cities.columns if "_y" in c], 1),
-        get_places_id.now(config),
-        matchs,
+    # Fix for default places ids
+    df = df.drop(["state_name", "health_system_region", "city_name"], axis=1).merge(
+        places_ids, on=["city_id", "state_id"]
     )
-    # cities["city_norm"] = cities["city_name"].apply(treat_text)
 
-    time_cols = [c for c in cities.columns if "last_updated" in c]
-    cities[time_cols] = cities[time_cols].apply(pd.to_datetime)
+    # Fix date types
+    time_cols = [c for c in df.columns if "last_updated" in c]
+    df[time_cols] = df[time_cols].apply(pd.to_datetime)
 
-    cities[["number_beds", "number_ventilators"]] = cities[
+    df[["number_beds", "number_ventilators"]] = df[
         ["number_beds", "number_ventilators"]
     ].fillna(0)
 
-    cities["author_number_beds"] = "DataSUS"  # config[country]["health"]["source"]
-    cities["author_number_ventilators"] = "DataSUS"
+    # Add DataSUS author
+    df["author_number_beds"] = config[country]["cnes"]["source"]
+    df["author_number_ventilators"] = config[country]["cnes"]["source"]
 
-    return cities
+    return df
 
 
 TESTS = {

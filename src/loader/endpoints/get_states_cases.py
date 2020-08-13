45494b69dedd1881c2e53b111b89e3c2b0544986
Dandara Sousa <dandaramcsousa@gmail.com>
2020-08-13 13:56:15 -0300

    problema no merge

--- src/loader/endpoints/get_states_cases.py
+++ src/loader/endpoints/get_states_cases.py
@@ -29,69 +29,71 @@ def now(config, country="br"):
         # Get data & clean table
         df = (
             download_brasilio_table(config["br"]["cases"]["url"])
-            .query("place_type == 'state'")
+            .query("place_type == 'city'")
             .dropna(subset=["city_ibge_code"])
             .fillna(0)
             .rename(columns=config["br"]["cases"]["rename"])
             .assign(last_updated=lambda x: pd.to_datetime(x["last_updated"]))
-            .sort_values(["city_id", "state_id", "last_updated"])
+            .groupby(["state_id","last_updated"])
+            .agg(
+                estimated_population_2019 = ('estimated_population_2019', sum),
+                confirmed_cases = ("confirmed_cases", sum),
+                deaths = ("deaths", sum),
+                daily_cases = ("daily_cases", sum),
+                new_deaths = ("new_deaths", sum)
+            )
+            .reset_index()
+            .sort_values(["state_id", "last_updated"])
         )
 
         # Fix places_ids
         places_ids = get_places_id.now(config)
-        
+
         df = (
-            df.drop(["city_name","city_id"]
-            .merge(
+            df.merge(
                 places_ids[
                     [
-                        "city_id",
-                        "city_name",
-                        "health_region_name",
-                        "health_region_id",
                         "state_name",
                         "state_num_id",
                         "state_id"
                     ]
                 ],
-                on="state_id",
+                left_on="state_id"
             )
         )
-
-        print(df.columns)
-
+        print(df.info())
+        
         # Correct negative values, get infectious period cases and get median of new cases
-        df = (
-            df.groupby("state_id")
-            .apply(correct_negatives)
-            .pipe(
-                get_infectious_period_cases,
-                infectious_period,
-                config["br"]["cases"],
-                "state_id",
-            )
-            .rename(columns=config["br"]["cases"]["rename"])
-        )
+        #df = (
+        #    df.groupby("state_id")
+        #    .apply(correct_negatives)
+        #    .pipe(
+        #        get_infectious_period_cases,
+        #        infectious_period,
+        #        config["br"]["cases"],
+        #        "state_id",
+        #    )
+        #    .rename(columns=config["br"]["cases"]["rename"])
+        #)
 
-        print(df.head(3))
 
         # Get indicators of mavg & growth
-        df = get_mavg_indicators(df, "daily_cases", place_id="state_id")
-        df = get_mavg_indicators(df, "new_deaths", place_id="state_id")
+        #df = get_mavg_indicators(df, "daily_cases", place_id="state_id")
+        #df = get_mavg_indicators(df, "new_deaths", place_id="state_id")
 
         # Get notification rates & active cases on date
-        df = df.merge(
-            get_notification_rate.now(df, "state_num_id"),
-            on=["state_num_id", "last_updated"],
-            how="left",
-        ).assign(
-            active_cases=lambda x: np.where(
-                x["notification_rate"].isnull(),
-                np.nan, #round(x["infectious_period_cases"], 0),
-                round(x["infectious_period_cases"] / x["notification_rate"], 0),
-            ),
-            state_id=lambda x: x["state_id"].astype(int),
-        )
+        #df = df.merge(
+        #    get_notification_rate.now(df, "state_num_id"),
+        #    on=["state_num_id", "last_updated"],
+        #    how="left",
+        #).assign(
+        #    active_cases=lambda x: np.where(
+        #        x["notification_rate"].isnull(),
+        #        np.nan, #round(x["infectious_period_cases"], 0),
+        #        round(x["infectious_period_cases"] / x["notification_rate"], 0),
+        #    ),
+        #    state_id=lambda x: x["state_id"].astype(int),
+        #)
 
     return df
 

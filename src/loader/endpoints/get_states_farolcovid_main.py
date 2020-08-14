054e559a764cf79204423325adce19edb51320ad
fernandascovino <fscovinom@gmail.com>
2020-08-14 13:27:09 +0000

    change to state cases

--- src/loader/endpoints/get_states_farolcovid_main.py
+++ src/loader/endpoints/get_states_farolcovid_main.py
@@ -2,8 +2,14 @@ import pandas as pd
 import numpy as np
 import datetime as dt
 import yaml
+from math import ceil
 
-from endpoints import get_cases, get_states_rt, get_health
+from endpoints import (
+    get_states_cases,
+    get_states_rt,
+    get_health,
+    get_health_region_farolcovid_main,
+)
 from endpoints.get_cities_farolcovid_main import (
     get_situation_indicators,
     get_control_indicators,
@@ -14,23 +20,44 @@ from endpoints.get_cities_farolcovid_main import (
 from endpoints.helpers import allow_local
 
 
+# TODO: alert by regions
+# def _get_population_alert(group, mean_pop):
+#     # group["population"].cumsum().searchsorted(mean_pop[group.index[1]]).reset()
+#     # .query(f"population < {mean_pop}")
+#     # .drop_duplicated(["state_num_id"], keep="first"))
+
+
+
+# def get_weighted_level(regions):
+
+#     mean_pop = regions.groubpy("state_num_id")['population'].sum()/2
+
+#     max_regions_alert = regions.groubpy("state_num_id")["overall_alert"].apply(lambda x: ceil(np.quantile(x, 0.5)))
+
+#     max_population_alert = (regions.sort_values(["overall_alert", "state_num_id"])
+#     .groubpy(["overall_alert", "state_num_id"]).apply(lambda group: _get_population_alert(group, mean_pop), axis=1)
+
+#     return pd.concat([max_regions_alert, max_population_alert])["overall_alert"].max(level=0)
+
+
+
 @allow_local
 def now(config):
 
     # Get last cases data
     cases = (
-        get_cases.now(config, "br")
+        get_states_cases.now(config, "br")
         .dropna(subset=["active_cases"])
         .assign(last_updated=lambda df: pd.to_datetime(df["last_updated"]))
     )
-    cases = cases.loc[cases.groupby("city_id")["last_updated"].idxmax()].drop(
-        config["br"]["cases"]["drop"] + ["state_num_id", "health_region_id"], 1
-    )
+    # cases = cases.loc[cases.groupby("city_id")["last_updated"].idxmax()].drop(
+    #     config["br"]["cases"]["drop"] + ["state_num_id", "health_region_id"], 1
+    # )
 
     # Merge resource data
     df = get_health.now(config, "br")[
         config["br"]["simulacovid"]["columns"]["cnes"]
-    ].merge(cases, on="city_id", how="left")
+    ].merge(cases, on="state_num_id", how="left")
 
     df = (
         df.sort_values("state_num_id")
@@ -45,11 +72,10 @@ def now(config):
     # TODO: get_cases => get_states_cases / mudar indicadores de situacao + add trust (notification_rate)!
     df = get_situation_indicators(
         df,
-        data=get_city_cases.now(config),
+        data=get_states_cases.now(config),
         place_id="state_num_id",
         rules=config["br"]["farolcovid"]["rules"],
         classify="situation_classification",
-        growth=None,  # -> "situation_growth" depois do update na tabela de casos
     )
 
     df = get_control_indicators(
@@ -58,7 +84,6 @@ def now(config):
         place_id="state_num_id",
         rules=config["br"]["farolcovid"]["rules"],
         classify="control_classification",
-        growth=None,  # "control_growth",
     )
 
     df = get_capacity_indicators(
@@ -71,14 +96,17 @@ def now(config):
 
     df = get_trust_indicators(
         df,
-        data=get_cases.now(config),
+        data=get_states_cases.now(config),
         place_id="state_num_id",
         rules=config["br"]["farolcovid"]["rules"],
         classify="trust_classification",
-        growth=None,  # "trust_growth",
     )
 
     cols = [col for col in df.columns if "classification" in col]
+
+    # TODO: Overall alert - max of cumulative regions in level
+    # regions = get_weighted_level(get_health_region_farolcovid_main.now(config))
+
     df["overall_alert"] = df.apply(
         lambda row: get_overall_alert(row[cols]), axis=1
     )  # .replace(config["br"]["farolcovid"]["categories"])

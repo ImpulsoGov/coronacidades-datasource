f247c35796ca9d06a6706c63941db41563ed8a68
Gabrielle Arruda <gabriellearrudac@gmail.com>
2021-02-19 19:06:37 -0300

    Correct owid dat (#128)
    * Update not-found.html
    
    * Fix apimaps (#123)
    
    * Update endpoints.yaml
    
    * Update endpoints.yaml
    
    * Update endpoints.yaml
    
    * Makes URL request
    
    * Update endpoints.yaml
    
    Co-authored-by: Jancer Gomes <jancer@impulsogov.org>

--- src/loader/endpoints/get_owid_heatmap.py
+++ src/loader/endpoints/get_owid_heatmap.py
@@ -10,8 +10,14 @@ def _get_rolling_amount(grp, time, data_col="last_updated", col_to_roll="deaths"
 @allow_local
 def now(config=None):
 
+    ### Prevents bad HTTP Response (403)
+    from urllib.request import Request, urlopen
+    req = Request("https://covid.ourworldindata.org/data/owid-covid-data.csv")
+    req.add_header('User-Agent', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:77.0) Gecko/20100101 Firefox/77.0')
+    content = urlopen(req)
+
     df = (
-        pd.read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv")
+        pd.read_csv(content)
         .dropna(subset=["new_deaths"])[
             ["iso_code", "date", "total_deaths", "new_deaths"]
         ]
@@ -27,6 +33,7 @@ def now(config=None):
     ).apply(lambda x: _get_rolling_amount(x, 5, "date", "new_deaths"))
 
     return df
+    print(df.describe())
 
 
 TESTS = {
